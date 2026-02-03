cb427230ef7a55b5833657b6dea3569cb8947913
Shivansh Mishra <shivanshmishra209310@gmail.com>
2026-02-03 14:56:19 +0530

    TopBrains Added

---
+++ Saturday-Assessment/BankTestProject/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class UnitTest
+{
+    [Test]
+    public void Test_Deposit_ValidAmount()
+    {
+        Program account = new Program(1000);
+        account.Deposit(500);
+
+        Assert.That(account.Balance, Is.EqualTo(1500));
+    }
+
+    [Test]
+    public void Test_Deposit_NegativeAmount()
+    {
+        Program account = new Program(1000);
+
+        Exception ex = Assert.Throws<Exception>(() => account.Deposit(-100));
+
+        Assert.That(ex.Message, Is.EqualTo("Deposit amount cannot be negative"));
+    }
+
+    [Test]
+    public void Test_Withdraw_ValidAmount()
+    {
+        Program account = new Program(1000);
+        account.Withdraw(300);
+
+        Assert.That(account.Balance, Is.EqualTo(700));
+    }
+
+    [Test]
+    public void Test_Withdraw_InsufficientFunds()
+    {
+        Program account = new Program(500);
+
+        Exception ex = Assert.Throws<Exception>(() => account.Withdraw(1000));
+
+        Assert.That(ex.Message, Is.EqualTo("Insufficient funds."));
+    }
+}
